1515a7f863a038d7d459048c878a19e7b6562108
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2022-03-20 15:34:10 +1100

    Started work on ps5 scanner

--- index.ts
+++ index.ts
@@ -1,8 +1,25 @@
+//Imports
 import {Client, Intents} from "discord.js";
 import Dotenv from "dotenv";
 Dotenv.config();
 
+//Scanner Imports
+import Ps5BigW from "./scanners/ps5BigW";
+
+//Discord Client Setup
 const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
-client.once('ready', () => console.log('Discord client is ready.'));
+client.once('ready', () => {
+    console.log("Discord client is ready.")
+
+    //Actives scanners as specified in .env
+    if(process.env.PS5BIGW && process.env.PS5BIGW_CHANNEL_ID) {
+        const ps5BigW = new Ps5BigW(client, process.env.PS5BIGW_CHANNEL_ID);
+        setInterval(ps5BigW.scan, 10000);
+    }
+});
+
+client.on("interactionCreate", interaction => {
+
+});
 
 client.login(process.env.DISCORD_TOKEN);
