d7250d9d619f1f63d9c95cab5917d73a873c2cc0
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2022-12-07 23:45:12 +1100

    FB NewScan Function

--- scanners/facebook.ts
+++ scanners/facebook.ts
@@ -64,6 +64,47 @@ class Facebook {
             await browser.disconnect();
             await browser.close();
         }
+    }
+
+    async newScan(page: puppeteer.Page) {
+        try {
+            let cursor = FacebookQuery.find().cursor();
+            for(let item = await cursor.next(); item != null; item = await cursor.next()) { 
+                try {                   
+                    await page.goto(item.name);
+                    await page.waitForTimeout(Math.random()*10000 + 5000); //Waits before continuing. (Trying not to get IP banned)
+
+                    let res = await page.$('div.x9f619.x78zum5.xdt5ytf.x1qughib.x1rdy4ex.xz9dl7a.xsag5q8.xh8yej3.xp0eagm.x1nrcals')
+                    if(!res) continue;
+                    let results = await res.$$eval('div.x1gslohp.xkh6y0r', nodes => nodes.map(n => n.textContent));
+                    //[ 'A$450', 'item name', 'Sydney, NSW' ] NOTE: 'A$290A$350' is how a discount will appear
+                    if(!results) continue;
+                    let foundPrice = parseInt(results[0]?.split('A$')[1].replace(',', '') ?? '9999999', 10); //The text starts with A$, so [0] will be nothing, while [1] will be the price, and [2] will be the original price if discounted
+                    let foundName = results[1];
+                    //console.log(`Price ${foundPrice}, name ${foundName}`)
+
+                    if(foundName !== undefined && foundPrice !== undefined && foundName != item.lastItemFound) {
+                        this.client.channels.fetch(this.channelId)
+                            .then(channel => <TextChannel>channel)
+                            .then(channel => {
+                                if(channel) channel.send(`<@&${this.roleId}> Please know that a ${foundName} priced at $${foundPrice} is available at ${item.name}`);
+                            })
+                            .catch(e => console.error(e))
+                        if(foundName != undefined) {
+                            item.lastItemFound = foundName;
+                            item.save();
+                        }
+                    }
+
+                }
+                catch (e) {
+                    console.error(e);
+                }
+            }
+        }
+        catch(e) {
+            console.error(e);
+        }
     }   
 }
 
