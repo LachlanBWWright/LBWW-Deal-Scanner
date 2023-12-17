79de7f47c59a4e4de1641bbf9e6d45e2694adadc
LachlanBWWright <lachlanbwwright@gmail.com>
2023-12-18 00:28:03 +1100

    Created globals schema

---
+++ schema/globals.ts
@@ -0,0 +1,82 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface globalsInterface {
+  BOT_CLIENT_ID: number;
+  CASH_CONVERTERS: boolean;
+  CASH_CONVERTERS_CHANNEL_ID: number | null;
+  CASH_CONVERTERS_ROLE_ID: number | null;
+  COMMAND_PERMISSION_ROLE_ID: number | null;
+  CS_CHANNEL_ID: number | null;
+  CS_ITEMS: boolean;
+  CS_MARKET_CHANNEL_ID: number | null;
+  CS_MARKET_ROLE_ID: number | null;
+  CS_ROLE_ID: number | null;
+  DISCORD_GUILD_ID: number | null;
+  DISCORD_TOKEN: string | null;
+  EBAY: boolean;
+  EBAY_CHANNEL_ID: number | null;
+  EBAY_ROLE_ID: number | null;
+  FACEBOOK: boolean;
+  FACEBOOK_CHANNEL_ID: number | null;
+  FACEBOOK_ROLE_ID: number | null;
+  GUMTREE: boolean;
+  GUMTREE_CHANNEL_ID: number | null;
+  GUMTREE_ROLE_ID: number | null;
+  MONGO_URI: string | null;
+  PS5BIGW: boolean;
+  PS5_CHANNEL_ID: number | null;
+  PS5_ROLE_ID: number | null;
+  SALVOS: boolean;
+  SALVOS_CHANNEL_ID: number | null;
+  SALVOS_ROLE_ID: number | null;
+  STEAM_QUERY: boolean;
+  STEAM_QUERY_CHANNEL_ID: number | null;
+  STEAM_QUERY_ROLE_ID: number | null;
+  XBOXBIGW: boolean;
+  XBOX_CHANNEL_ID: number | null;
+  XBOX_ROLE_ID: number | null;
+}
+
+const GlobalsSchema: Schema<globalsInterface> = new Schema<globalsInterface>({
+  BOT_CLIENT_ID: { type: Number, required: true },
+  CASH_CONVERTERS: { type: Boolean, required: true },
+  CASH_CONVERTERS_CHANNEL_ID: { type: Number, default: null },
+  CASH_CONVERTERS_ROLE_ID: { type: Number, default: null },
+  COMMAND_PERMISSION_ROLE_ID: { type: Number, default: null },
+  CS_CHANNEL_ID: { type: Number, default: null },
+  CS_ITEMS: { type: Boolean, required: true },
+  CS_MARKET_CHANNEL_ID: { type: Number, default: null },
+  CS_MARKET_ROLE_ID: { type: Number, default: null },
+  CS_ROLE_ID: { type: Number, default: null },
+  DISCORD_GUILD_ID: { type: Number, default: null },
+  DISCORD_TOKEN: { type: String, default: null },
+  EBAY: { type: Boolean, required: true },
+  EBAY_CHANNEL_ID: { type: Number, default: null },
+  EBAY_ROLE_ID: { type: Number, default: null },
+  FACEBOOK: { type: Boolean, required: true },
+  FACEBOOK_CHANNEL_ID: { type: Number, default: null },
+  FACEBOOK_ROLE_ID: { type: Number, default: null },
+  GUMTREE: { type: Boolean, required: true },
+  GUMTREE_CHANNEL_ID: { type: Number, default: null },
+  GUMTREE_ROLE_ID: { type: Number, default: null },
+  MONGO_URI: { type: String, default: null },
+  PS5BIGW: { type: Boolean, required: true },
+  PS5_CHANNEL_ID: { type: Number, default: null },
+  PS5_ROLE_ID: { type: Number, default: null },
+  SALVOS: { type: Boolean, required: true },
+  SALVOS_CHANNEL_ID: { type: Number, default: null },
+  SALVOS_ROLE_ID: { type: Number, default: null },
+  STEAM_QUERY: { type: Boolean, required: true },
+  STEAM_QUERY_CHANNEL_ID: { type: Number, default: null },
+  STEAM_QUERY_ROLE_ID: { type: Number, default: null },
+  XBOXBIGW: { type: Boolean, required: true },
+  XBOX_CHANNEL_ID: { type: Number, default: null },
+  XBOX_ROLE_ID: { type: Number, default: null },
+});
+
+const GlobalsQuery = mongoose.model<globalsInterface>(
+  "GlobalsQuery",
+  GlobalsSchema
+);
+
+export default GlobalsQuery;
