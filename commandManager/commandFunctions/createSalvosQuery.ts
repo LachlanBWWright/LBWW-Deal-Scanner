ecde55e21d755c059b90df9119e1236b3f42b39d
Lachlan Wright <79518204+LachlanBWWright@users.noreply.github.com>
2024-01-12 10:15:05 +1100

    Fixed salvos query to force newstFirst sorting

--- commandManager/commandFunctions/createSalvosQuery.ts
+++ commandManager/commandFunctions/createSalvosQuery.ts
@@ -3,7 +3,7 @@ import SalvosQuery from "../../schema/salvosQuery.js";
 
 export default async function (interaction: ChatInputCommandInteraction) {
   let query = interaction.options.getString("query") || "placeholder";
-  let search = new URL(query);
+  let search = new URL(query + "&sorting=newestFirst");
   const searchString = search.toString();
   if (
     searchString.includes("https://www.salvosstores.com.au/shop?search=") ||
