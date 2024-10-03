78092f27bf4b71233dc5696cc6aff6a1c29e8e96
LachlanBWWright <lachlanbwwright@gmail.com>
2024-10-04 00:53:47 +1000

    Salvos query fix

--- commandManager/commandList/createSalvosQuery.ts
+++ commandManager/commandList/createSalvosQuery.ts
@@ -7,17 +7,17 @@ export default new SlashCommandBuilder()
     option
       .setName("query")
       .setDescription("The name of the query.")
-      .setRequired(true)
+      .setRequired(true),
   )
   .addNumberOption((option) =>
     option
       .setName("minprice")
       .setDescription("Enter the minimum price for a notification.")
-      .setRequired(false)
+      .setRequired(true),
   )
   .addNumberOption((option) =>
     option
       .setName("maxprice")
       .setDescription("Enter the maximum price for a notification.")
-      .setRequired(false)
+      .setRequired(true),
   );
