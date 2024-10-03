fe09e3185dd526a5bd016d38d7eb3e51f7a35a48
LachlanBWWright <lachlanbwwright@gmail.com>
2024-10-04 02:15:30 +1000

    Removed logging

--- scanners/siteScanners/salvos.ts
+++ scanners/siteScanners/salvos.ts
@@ -27,7 +27,7 @@ export async function scanSalvos() {
         tracking: {},
         values: {
           q: item.name,
-          resultsPerPage: "20",
+          resultsPerPage: "10",
           filter: `(price >= ${item.minPrice ?? "0"} AND price <= ${
             item.maxPrice ?? "99999"
           })`,
@@ -51,7 +51,6 @@ export async function scanSalvos() {
 
   let notificationSent = false; //Don't spam notifications if multiple new items
 
-  console.log("Salvos log");
   for (let i = 0; i < res.data.searchResponse.results.length; i++) {
     const data = res.data.searchResponse.results[i];
     const name = data.values.name.single;
@@ -59,17 +58,12 @@ export async function scanSalvos() {
     const id = data.values.id.single;
     const slug = data.values.slug.single; // E.G. name = "Xbox One S 1TB Console" slug = "xbox-one-s-1tb-console"
     const price = data.values.price.single;
-    console.log(i, name, image, price);
 
     if (!name || !image || !slug || !id || !price) return;
 
     if (price > item.maxPrice || price < item.minPrice) return;
 
-    if (
-      (await checkIfNew(id, SCANNER.SALVOS)) &&
-      i <= 10 &&
-      !notificationSent
-    ) {
+    if ((await checkIfNew(id, SCANNER.SALVOS)) && i <= 5 && !notificationSent) {
       sendToChannel(
         globals.SALVOS_CHANNEL_ID,
         `<@&${
